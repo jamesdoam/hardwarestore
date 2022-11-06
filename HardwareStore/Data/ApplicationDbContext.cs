c83c9ee61387617e9816f23e3ff9d508ea9921eb
jamesdoam <jamesdoam@gmail.com>
2022-11-06 22:38:02 +1100

    Configure program.cs to use ApplicationDbContext

--- HardwareStore/Data/ApplicationDbContext.cs
+++ HardwareStore/Data/ApplicationDbContext.cs
@@ -1,6 +1,15 @@
-﻿namespace HardwareStoreWeb.Data
+﻿using HardwareStoreWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HardwareStoreWeb.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext:DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
     }
 }
